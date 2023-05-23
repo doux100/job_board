fa4bf687ac2e1f0f041e9e3018226a2485968cbf
doux100 <93144348+doux100@users.noreply.github.com>
2023-05-23 16:39:50 +0300

    statics

--- project/settings.py
+++ project/settings.py
@@ -23,14 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'aghhbJOPPHOPUIOAHdfsdpoi^^%#$#&*))*(__&dbbsfdsagdfduyag^%()(_&^&aavdbasjidaa57ASDDASHVD566234BASBASDU21234fd))'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
-CSRF_COOKIE_SECURE=True
-SESSION_COOKIE_SECURE=True
-SECURE_SSL_REDIRECT=True
-SECURE_HSTS_SECONDS=31536000
-SECURE_HSTS_INCLUDE_SUBDOMAINS=True
-SECURE_HSTS_PRELOAD=True
+# CSRF_COOKIE_SECURE=True
+# SESSION_COOKIE_SECURE=True
+# SECURE_SSL_REDIRECT=True
+# SECURE_HSTS_SECONDS=31536000
+# SECURE_HSTS_INCLUDE_SUBDOMAINS=True
+# SECURE_HSTS_PRELOAD=True
 
 
 # Application definition
