5701121a7a0dc46c2c35c3aaa18abc203aeba1c4
doux100 <93144348+doux100@users.noreply.github.com>
2023-05-25 16:33:04 +0300

    change debug true

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'aghhbJOPPHOPUIOAHdfsdpoi^^%#$#&*))*(__&dbbsfdsagdfduyag^%()(_&^&aavdbasjidaa57ASDDASHVD566234BASBASDU21234fd))'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 # CSRF_COOKIE_SECURE=True
 # SESSION_COOKIE_SECURE=True
