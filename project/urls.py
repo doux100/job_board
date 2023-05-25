3ab9b9e5212b1d5e82f4b6b023bc31d97d9815ca
doux100 <93144348+doux100@users.noreply.github.com>
2023-05-25 18:13:52 +0300

    adding grappelli to requirements.tst

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path, include
 from django.contrib import admin
-import grappelli
 """
 URL configuration for project project.
 
