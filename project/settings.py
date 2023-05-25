cc824505e1d558c1129cb1739720b1fe74d5e60c
doux100 <93144348+doux100@users.noreply.github.com>
2023-05-25 17:50:58 +0300

    delete django_extensions from setting

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'accounts',
     'jazzmin',
-    "django_extensions",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
